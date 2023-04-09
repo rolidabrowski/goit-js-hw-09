8338f4229fbf2ac6757a23cfec09eb866bbedea2
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-04-09 21:18:04 +0200

    added task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const output = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    output.style.backgroundColor = color;
+  }, 1000);
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled', '');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled', '');
+  stopBtn.setAttribute('disabled', '');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
