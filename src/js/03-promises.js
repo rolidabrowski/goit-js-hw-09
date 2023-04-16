ea67d5e27591071c531fc9c6d0ae5bd064918c1c
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-04-17 00:26:39 +0200

    added task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
 }
+
+const showPromise = (amount, step, time, current = 1) => {
+  if (current > amount) return;
+  createPromise(current, time)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+  time += step;
+  setTimeout(() => showPromise(amount, step, time, current + 1), step);
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delay = event.currentTarget.elements.delay;
+  const step = event.currentTarget.elements.step;
+  const amount = event.currentTarget.elements.amount;
+  setTimeout(
+    () => showPromise(+amount.value, +step.value, +delay.value),
+    +delay.value
+  );
+});
